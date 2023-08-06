c39d19c1b0dbe531b76b848c0fcee945424aac66
Vladimir Danilov <dev.danilovl@gmail.com>
2023-08-06 09:14:46 +0200

    Update gradle to new version 232.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.21"
     id("org.jetbrains.intellij") version "1.10.0"
     id("org.jetbrains.changelog") version "1.3.1"
     id("org.jetbrains.qodana") version "0.1.13"
